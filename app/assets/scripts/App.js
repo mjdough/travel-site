363af12a2496ed0ee1334d6a1ba8d28f27cdb1e1
mjdough <doughmj@outlook.com>
2016-09-17 11:57:59 -0400

    save local changes before merge

---
+++ app/assets/scripts/App.js
@@ -0,0 +1,14 @@
+function Person(fullName, favColor) {
+  this.name = fullName;
+  this.favoriteColor = favColor;
+  this.greet = function() {
+    console.log("Hello!, my name is " + this.name + " and my favorite color is " + this.favoriteColor + "." );
+  }
+}
+
+var john = new Person("John Doe", "blue");
+var jane = new Person("Jane Smith", "green");
+
+
+john.greet();
+jane.greet();
